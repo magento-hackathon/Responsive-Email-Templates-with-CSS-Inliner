5092c3482e400555deb12bfcc7dca114af5cc75e
Kalen Jordan <kalen@sparcksoft.com>
2014-02-04 07:31:07 -0800

    Fix the model names in abstract template class

--- app/code/community/Hackathon/ResponsiveEmail/Model/Email/Template/Abstract.php
+++ app/code/community/Hackathon/ResponsiveEmail/Model/Email/Template/Abstract.php
@@ -3,7 +3,9 @@
 if ((string)Mage::getConfig()->getModuleConfig('Aschroder_SMTPPro')->active == 'true') {
     class Hackathon_ResponsiveEmail_Model_Email_Template_Abstract extends Aschroder_SMTPPro_Model_Email_Template { }
 } elseif ((string)Mage::getConfig()->getModuleConfig('Ebizmarts_Mandrill')->active == 'true') {
-    class KJ_AutoEmail_Model_Email_Template_Abstract extends Ebizmarts_Mandrill_Model_Email_Template { }
+    class Hackathon_ResponsiveEmail_Model_Email_Template_Abstract extends Ebizmarts_Mandrill_Model_Email_Template { }
+} elseif ((string)Mage::getConfig()->getModuleConfig('Hackathon_ResponsiveEmail')->active == 'true') {
+    class Hackathon_ResponsiveEmail_Model_Email_Template_Abstract extends Hackathon_ResponsiveEmail_Model_Email_Template { }
 } else {
-    class KJ_AutoEmail_Model_Email_Template_Abstract extends Mage_Core_Model_Email_Template { }
+    class Hackathon_ResponsiveEmail_Model_Email_Template_Abstract extends Mage_Core_Model_Email_Template { }
 }
